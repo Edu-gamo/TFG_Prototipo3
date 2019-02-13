cecaa60b64070e8f096cbca3a2cd63bafa1d324a
agonzalez <adriagonzalez@enti.cat>
2019-02-13 16:19:15 +0100

    New features
    Quan algo cau al terra torna a la posicio inicial + Fade a negre progressiu

---
+++ Assets/ReturnToStartPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToStartPosition : MonoBehaviour
+{
+    Vector3 startingPos;
+    Vector3 resetVel = new Vector3(0.0f,0.0f,0.0f);
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+        startingPos.y = startingPos.y + 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = startingPos;
+        rb.velocity = resetVel;
+    }
+}
